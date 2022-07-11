a64496b6bc756b97e85ae5bb3a59b837f87bef9b
ManishaSadasivan <54832964+ManishaSadasivan@users.noreply.github.com>
2022-07-12 00:58:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
-import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import "./style/dark.scss";
+
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
